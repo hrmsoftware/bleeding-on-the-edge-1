1dcfd99ac59ec286562dc7c47ab907ef397c5a83
Jeff Erlandsson <jeff.erlandsson@gmail.com>
2014-04-10 14:35:51 +0200

    Add win / guess ratio to formatter.

--- casino/file-leaderboard/src/main/java/se/hrmsoftware/guess/leaderboard/internal/SnapshotFormatter.java
+++ casino/file-leaderboard/src/main/java/se/hrmsoftware/guess/leaderboard/internal/SnapshotFormatter.java
@@ -6,16 +6,16 @@ package se.hrmsoftware.guess.leaderboard.internal;
 public class SnapshotFormatter {
 	public static String format(final Snapshot snapshot) {
         StringBuilder s = new StringBuilder();
-        s.append(String.format("+------------------------------------+\n"));
-        s.append(String.format("| Current: %-26d|\n", snapshot.getCurrentGames()));
-        s.append(String.format("| Games played: %-21d|\n", snapshot.getPlayedGames()));
-        s.append(String.format("+------------------------------------+\n"));
-        s.append(String.format("| Player    | Wins      | Guesses    |\n"));
-        s.append(String.format("+------------------------------------+\n"));
+        s.append(String.format("+-----------------------------------------------+\n"));
+        s.append(String.format("| Current: %-37d|\n", snapshot.getCurrentGames()));
+        s.append(String.format("| Games played: %-32d|\n", snapshot.getPlayedGames()));
+        s.append(String.format("+-----------------------------------------------+\n"));
+        s.append(String.format("| Player    | Wins      | Guesses    | Ratio    |\n"));
+        s.append(String.format("+-----------------------------------------------+\n"));
         for(Snapshot.PlayerStats stats : snapshot.getPlayerStats()) {
-            s.append(String.format("| %-10s| %-10d| %-11d|\n", stats.getPlayer(), stats.getWins(), stats.getGuesses()));
+            s.append(String.format("| %-10s| %-10d| %-11d| %-9.2f|\n", stats.getPlayer(), stats.getWins(), stats.getGuesses(), ((double)stats.getWins() / stats.getGuesses())*100));
         }
-        s.append(String.format("+------------------------------------+\n"));
+        s.append(String.format("+-----------------------------------------------+\n"));
         return s.toString();
 	}
 }
